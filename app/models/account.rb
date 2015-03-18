592f309ee457c44f4e026afca595b9ea6241b2eb
pethl <pethicklisa@gmail.com>
2015-03-18 20:02:08 +0000

    bring minor format improvements and bug fix where no ticker on accounts show

--- app/models/account.rb
+++ app/models/account.rb
@@ -12,8 +12,12 @@ class Account < ActiveRecord::Base
   end
   
   def self.get_share_price(ticker)
+    if ticker.blank?
+    return 
+  else
     gdp = Quandl::Dataset.get(ticker).start('2015-03-04')
     gdp.data[0][1]
+  end
   end
   
   def self.get_share_count(account)
